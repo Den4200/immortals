ad9b9bceb6f202f3495cfa85578892ca9b427105
F4zi <fazbosoreq@gmail.com>
2020-02-03 16:53:10 +0200

    Unlimited gravity hotfix

--- immortals/core/player.py
+++ immortals/core/player.py
@@ -4,17 +4,19 @@ import pygame
 
 from .playerdata import PlayerData
 
+from .settings import GRAVITY, SPEED
+
 
 class Player(pygame.sprite.Sprite):
 
     def __init__(
-        self, 
-        x: int, y: int, 
-        width: int, height: int, 
-        color: Tuple[int, int, int],
-        screen_height: int,
-        screen_width: int,
-        map_arena
+            self,
+            x: int, y: int,
+            width: int, height: int,
+            color: Tuple[int, int, int],
+            screen_height: int,
+            screen_width: int,
+            map_arena
     ) -> None:
 
         super(Player, self).__init__()
@@ -25,7 +27,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        
+
         self.height = height
         self.width = width
         self.color = color
@@ -82,28 +84,29 @@ class Player(pygame.sprite.Sprite):
         if self.y_delta == 0:
             self.y_delta = 1
         else:
-            self.y_delta += 0.25
+            if self.y_delta < GRAVITY:
+                self.y_delta += 0.25
 
         if (
-            self.rect.y >= (self.screen_height - self.rect.height)
-            and self.y_delta >= 0
+                self.rect.y >= (self.screen_height - self.rect.height)
+                and self.y_delta >= 0
         ):
             self.y_delta = 0
             self.rect.y = self.screen_height - self.rect.height
-    
+
     def jump(self) -> None:
         self.rect.y += 1
         collided = pygame.sprite.spritecollide(self, self.map_arena.platform_list, False)
         self.rect.y -= 1
 
         if len(collided) > 0 or self.rect.bottom >= self.screen_height:
-            self.y_delta = -7.5
+            self.y_delta = -GRAVITY
 
     def move_left(self) -> None:
-        self.x_delta = -6
+        self.x_delta = -SPEED
 
     def move_right(self) -> None:
-        self.x_delta = 6
+        self.x_delta = SPEED
 
     def stop(self) -> None:
         self.x_delta = 0
