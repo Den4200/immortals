9c57290e5918fa30c2157f56724d39581ab0cdb4
F4zi <fazbosoreq@gmail.com>
2020-02-05 17:26:37 +0200

    Exceptions Re order

--- server/core/exceptions.py
+++ server/core/exceptions.py
@@ -1,9 +1,21 @@
 # Exceptions
 
 
-class MissingConfiguration(Exception):
+class ConfigurationException(Exception):
     pass
 
 
-class RoomIsFull(Exception):
+class MissingConfiguration(ConfigurationException):
+    pass
+
+
+class RoomException(Exception):
+    pass
+
+
+class RoomIsFull(RoomException):
+    pass
+
+
+class RoomDoesNotExist(RoomException):
     pass
